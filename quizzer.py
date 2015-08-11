fa040c8d5c878dcc8ee2caaa067120a9e8ce728a
Becky Sullivan <becsully@gmail.com>
2015-08-10 22:11:32 -0700

    quizzer added
    simple quizzer works!

--- quizzer.py
+++ quizzer.py
@@ -1,1 +1,73 @@
-__author__ = 'bsullivan'
+import csv
+from random import randint
+import linecache
+
+
+
+def picker():
+    with open("answers.csv", "r") as answers:
+        print "hello"
+
+
+def clue_to_dict(clue_str):
+    raw_list = clue_str.split(",")
+    # print raw_list
+    if len(raw_list) == 4:
+        clue_dict = {"Category": raw_list[0], "Value": raw_list[1], "Clue": raw_list[2], "Answer": raw_list[3]}
+    else:
+        category = []
+        clue = []
+        answer = raw_list[-1]
+        for i in range(len(raw_list)):
+            if raw_list[i][0] == "$" or raw_list[i] == "Final Jeopardy!":
+                value_index = i
+            else: pass
+        for i in range(0,value_index):
+            category.append(raw_list[i])
+        for i in range(value_index+1,len(raw_list)-1):
+            clue.append(raw_list[i])
+        final_category = ",".join(category)
+        final_clue = ",".join(clue)
+        clue_dict = {"Category": final_category, "Value": raw_list[value_index], "Clue": final_clue, "Answer": answer}
+    return clue_dict
+
+
+def printer(clue_dict):
+    print clue_dict["Category"] + " for " + clue_dict["Value"]
+    print clue_dict["Clue"]
+    raw_input("")
+    print clue_dict["Answer"]
+
+
+def random_question():
+    clue = None
+    while clue is None:
+        choice = 1
+        while choice % 2 != 0:
+            choice = randint(0,309764) # 608325 is the number of lines in wordlist.txt
+        clue = linecache.getline("answers.csv", choice).strip("\n")
+    return clue
+
+
+def test():
+    choice = 271596
+    clue = linecache.getline("answers.csv", choice)
+    return clue.strip("\n")
+
+
+if __name__ == "__main__":
+    print "JEOPARDY!"
+    print
+    print "Press enter to move the game along."
+    print 'Type "quit" to quit.'
+    print
+    while True:
+        try:
+            printer(clue_to_dict(random_question()))
+        except UnboundLocalError:
+            print "ERROR! moving along..."
+            printer(clue_to_dict(random_question()))
+        print "----------"
+        keep_going = raw_input("")
+        if keep_going == "quit":
+            break
--- scraper.py
+++ scraper.py
@@ -6,7 +6,55 @@ import requests
 from pprint import pprint
 
 
+def til_em(string):
+    answer = ""
+    while True:
+        for i in range(len(string)):
+            if string[i:i+5] != "</em>":
+                answer += string[i]
+            else:
+                break
+        break
+    return answer.encode('ascii','ignore')
+
+
+def text_from_code(soup_object):
+    print soup_object
+    try:
+        text = soup_object.string.encode('ascii', 'ignore')
+    except AttributeError:
+        try:
+            raw_text = soup_object.find("td", {"class": "clue_text"}).strings
+            text = ""
+            for string in raw_text:
+                text += string.encode('ascii','ignore')
+        except AttributeError:
+            text = "Sorry, there's been an error."
+    print text
+    return text
+
+
+def de_html(soup_object):
+    temp_clue = str(soup_object)
+    clue = ""
+    html_tag_closed = True
+    for i in range(len(temp_clue)-2):
+        if temp_clue[i] == "<":
+            html_tag_closed = False
+        elif temp_clue[i] == ">":
+            html_tag_closed = True
+        elif html_tag_closed:
+            try:
+                clue += temp_clue[i].encode('ascii', 'ignore')
+            except UnicodeDecodeError:
+                pass
+        else:
+            pass
+    return clue.encode('ascii', 'ignore')
+
+
 def scraper(url):
+    print "Scraping clues from " + url
     r = requests.get(url)
     soup = BeautifulSoup(r.text,'html.parser')
 
@@ -15,14 +63,8 @@ def scraper(url):
     for i in range(2):
         for j in range(6):
             name = "%i%i" % (i, j)
-            try:
-                categories[name] = raw_categories[i*6+j].string.encode('ascii', 'ignore')
-            except AttributeError:
-                raw_category = raw_categories[i*6+j].strings
-                categories[name] = ""
-                for string in raw_category:
-                    categories[name] += string.encode('ascii','ignore')
-    categories["FINAL JEOPARDY"] = raw_categories[12].string.encode('ascii', 'ignore')
+            categories[name] = de_html(raw_categories[i*6+j])
+    categories["FINAL JEOPARDY"] = de_html(raw_categories[12])
 
     q_list = []
     questions = soup.findAll("td", {"class": "clue"})
@@ -41,14 +83,7 @@ def scraper(url):
         try:
             raw_answer = (question.find("div"))["onmouseover"]
             raw_answer2 = (raw_answer.split('response">')[1]).encode('ascii', 'ignore')
-            answer = ""
-            while True:
-                for i in range(len(raw_answer2)):
-                    if raw_answer2[i:i+5] != "</em>":
-                        answer += raw_answer2[i]
-                    else:
-                        break
-                break
+            answer = til_em(raw_answer2)
         except TypeError:
             answer = "(none)"
 
@@ -58,31 +93,16 @@ def scraper(url):
             value = "Either this was a Daily Double, or the clue was not reached."
 
         try:
-            clue = (question.find("td", {"class": "clue_text"})).string.encode('ascii', 'ignore')
-            if clue == None:
-                temp_clue = (question.find("div"))["onmouseout"].split("', '")[2]
-                clue = ""
-                html_tag_closed = True
-                for i in range(len(temp_clue)-2):
-                    if temp_clue[i] == "<":
-                        html_tag_closed = False
-                    elif temp_clue[i] == ">":
-                        html_tag_closed = True
-                    elif html_tag_closed:
-                        clue += temp_clue[i]
-                    else: pass
+            clue = de_html(question.find("td", {"class": "clue_text"}))
         except AttributeError:
-            try:
-                raw_clue = question.find("td", {"class": "clue_text"}).strings
-                clue = ""
-                for string in raw_clue:
-                    clue += string.encode('ascii','ignore')
-            except AttributeError:
-                clue = "This question was not reached."
+            clue = "This question was not reached."
 
         if count == 60:
             category = categories["FINAL JEOPARDY"]
             value = "Final Jeopardy!"
+            final_raw = soup.find("table", {"class": "final_round"})
+            raw_answer = final_raw.find("div")["onmouseover"].split('correct_response')[1]
+            answer = til_em(raw_answer)[3:]
 
         question_dict = {"Clue": clue, "Value": value, "Answer": answer, "Category": category}
         q_list.append(question_dict)
@@ -94,12 +114,37 @@ def scraper(url):
 
 def saver(q_list):
     fieldnames = ["Category","Value","Clue","Answer"]
-    with open("answers.csv", "a") as jeopardy_csv:
+    with open("answers.csv", "ab") as jeopardy_csv:
         writer = csv.DictWriter(jeopardy_csv, fieldnames=fieldnames)
         for clue in q_list:
-            writer.writerow(clue)
+            if clue["Clue"] == "This question was not reached.":
+                pass
+            else:
+                writer.writerow(clue)
     jeopardy_csv.close()
     print "done with saving a set of questions!"
+
+
+def blank_remover():
+    count = 0
+    with open("new_answers2.csv", "rb") as original:
+        for line in original:
+            if count < 100228:
+                with open("new_answers3.csv", "ab") as replacement:
+                    replacement.write(line)
+                    replacement.close()
+                count += 1
+            else:
+                if count % 2 == 0:
+                    count += 1
+                    pass
+                else:
+                    with open("new_answers3.csv", "ab") as replacement:
+                        replacement.write(line)
+                        replacement.close()
+                        count += 1
+        print "done"
+    original.close()
 
 
 def base_scraper(url):
@@ -112,14 +157,24 @@ def base_scraper(url):
         links.append(show_link)
     for link in links:
         saver(scraper(link))
-    print "done done done with everything!"
 
 
 def test():
-    url = "http://www.j-archive.com/showgame.php?game_id=4589"
-    #url = "http://j-archive.com/showseason.php?season=30"
+    #url = "http://www.j-archive.com/showgame.php?game_id=3938"
+    #url = "http://j-archive.com/showseason.php?season=26"
     #base_scraper(url)
-    pprint(scraper(url))
+    #pprint(scraper(url))
+    blank_remover()
+
+
+def main():
+    for i in range(28,32):
+        url = "http://j-archive.com/showseason.php?season=%i" % i
+        print "Scraping Season #%i" % i
+        base_scraper(url)
+    print "done done done all scraping!"
+    blank_remover()
+    print "ALL DONE"
 
 
 if __name__ == "__main__":
